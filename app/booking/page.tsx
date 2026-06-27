a129e07568c34018a2387622900c9a0666a2cbdb
a day <waruneenaree@Mac-mini.local>
2026-06-27 19:42:54 +0700

    Update booking calendar

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -130,7 +130,7 @@ export default function BookingPage() {
                           margin: "0 auto",
                           padding: "4px 2px",
                           textAlign: "center",
-                          fontSize: "16px",
+                          fontSize: "15px",
                           textDecoration: "line-through",
                           fontWeight: 500,
                         }}
